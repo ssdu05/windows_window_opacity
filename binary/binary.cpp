7185e57545bfe067da9ff2a53bfcbf7824cb2e74
ssdu05 <ssdu@a1.net>
2024-10-09 22:03:36 +0200

    customizable opacity

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -1,4 +1,10 @@
 #include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+
+namespace globals {
+	static BYTE opacity = 255;
+};
 
 void SetWindowOpacity(HWND hwnd, BYTE opacity) {
     LONG_PTR style = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
@@ -7,11 +13,24 @@ void SetWindowOpacity(HWND hwnd, BYTE opacity) {
 }
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
-    SetWindowOpacity(hwnd, 230);
+    SetWindowOpacity(hwnd, globals::opacity);
     return TRUE;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 2) {
+        std::cout << "Usage: windopac.exe <opacity> -> 1 - 100" << std::endl;
+        return -1;
+    }
+    
+    if (argc == 2) {
+        int opacity{ std::atoi(argv[1]) };
+        std::cout << opacity << std::endl;
+        if (opacity != 0 && opacity > 0 && opacity < 99) {
+			globals::opacity = static_cast<BYTE>(opacity / 100.0 * 256.0);
+        }
+    }
+
     EnumWindows(EnumWindowsProc, 0);
     return 0;
 }
